2ba51ceeccfa143b0ff4d5f1d1ef503ca8ef2e54
Juan8516 <juan.ind85@gmail.com>
2023-10-26 14:21:55 -0500

    Se exporta componente formulario, se aplican estilos

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Formulario from '../components/Formulario'
 import { Navigate, useNavigate} from "react-router-dom" 
 
 function NuevoCliente() {
@@ -19,8 +20,17 @@ function NuevoCliente() {
         </button>
       </div>
 
-      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
-        <p>Formulario aqui</p>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <form>
+          <Formulario />
+
+          <input 
+              type="submit"
+              className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+              value={'Registrar Cliente'}
+
+          />
+        </form>
       </div>
     </>
   )
