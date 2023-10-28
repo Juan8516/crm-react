144c893500581a0c053d9f5632addfc27f925ebb
Juan8516 <juan.ind85@gmail.com>
2023-10-27 20:27:38 -0500

    Se crea funcion para comprobacion de email

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -8,18 +8,23 @@ export async function action( {request} ) {
   
   const datos = Object.fromEntries(formData)
 
+  const email = formData.get('email')
+
   //Validacion
   const errores = []
   if(Object.values(datos).includes('')) {
     errores.push('Todos los campos son obligatorios')
   }
 
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)) {
+    errores.push('El Email no es valido')
+  }
+
   //Retornar datos si hay errores
   if(Object.keys(errores).length) {
     return errores
   }
-
-  console.log(errores)
   
   return null
 }
@@ -49,6 +54,7 @@ function NuevoCliente() {
 
         <Form
            method='POST'
+           noValidate
         >
           <Formulario />
 
