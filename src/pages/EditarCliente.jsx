863f5b8404018c8026208fc9308c339cf1434494
Juan8516 <juan.ind85@gmail.com>
2023-10-30 22:48:40 -0500

    Se pasa error de busqueda, en funcion loader

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { obtenerCliente } from '../data/Clientes'
 
 export async function loader({ params }) {
-    console.log(params)
-    return null
+    const cliente = await obtenerCliente(params.clienteId)
+    if(Object.values(cliente).length === 0) {
+        throw new Response('', {
+            status: 404,
+            statusText: 'No hay Resultados'
+        })
+    }
+    return cliente
 }
 
 function EditarCliente() {
