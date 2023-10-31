1a29f40fb3e2fd65936b678a866ce36cdd2de6c7
Juan8516 <juan.ind85@gmail.com>
2023-10-30 20:11:31 -0500

    Se implementa try-catch en funcion agregar cliente para direccionar a componente clientes al finalizar formulario

--- src/data/Clientes.jsx
+++ src/data/Clientes.jsx
@@ -5,5 +5,16 @@ export async function obtenerClientes() {
 }
 
 export async function agregarCliente(datos) {
-    console.log(datos)
+    try {
+        const respuesta = await fetch(import.meta.env.VITE_API_URL, {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        await respuesta.json()
+    } catch (error) {
+        console.log(error)
+    }
 }
