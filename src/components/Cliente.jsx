6021b232da64b5dd3498c2e77cf2095ffabfadb3
Juan8516 <juan.ind85@gmail.com>
2023-11-01 14:20:31 -0500

    Se aplican estilos a botones editar y eliminar

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -7,7 +7,7 @@ function Cliente( {cliente} ) {
     const {nombre, empresa, email, telefono, id} = cliente
 
     return (
-        <tr className='border-b'>
+        <tr className='text-center border-b'>
             <td className='p-6 space-y-2'>
                 <p className='text-2xl text-gray-800'>{nombre}</p>
                 <p>{empresa}</p>
@@ -19,10 +19,10 @@ function Cliente( {cliente} ) {
                 <p className='text-gray-600'> <span className='text-gray-800 uppercase font-bold'>Telefono: </span>{telefono}</p>
             </td>
 
-            <td className='p-6 flex gap-3'>
+            <td className='mt-10 ml-20 flex gap-3'>
                 <button
                     type='button'
-                    className='text-blue-600 hover:text-blue-700 uppercase font-bold text-xs'
+                    className='p-3 text-blue-600 hover:text-blue-700 uppercase font-bold text-xs'
                     onClick={() => navigate(`/clientes/${id}/editar`)}
                 >
                     Editar
@@ -30,7 +30,7 @@ function Cliente( {cliente} ) {
 
                 <button
                     type='button'
-                    className='text-red-600 hover:text-red-700 uppercase font-bold text-xs'
+                    className='p-3 text-red-600 hover:text-red-700 uppercase font-bold text-xs'
                 >
                     ELiminar
                 </button>
