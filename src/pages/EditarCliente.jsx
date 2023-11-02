f91c398353da462c078af823ca4c1d5f1d2a27c2
Juan8516 <juan.ind85@gmail.com>
2023-11-01 20:33:17 -0500

    Se implementab hooks para cargar informacion y edicion de clientes

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { obtenerCliente } from '../data/Clientes'
+import { Form, useLoaderData, useNavigate } from 'react-router-dom'
+import Formulario from '../components/Formulario'
 
 export async function loader({ params }) {
     const cliente = await obtenerCliente(params.clienteId)
@@ -13,10 +15,45 @@ export async function loader({ params }) {
 }
 
 function EditarCliente() {
+
+  const navigate = useNavigate()
+  const cliente = useLoaderData()
+  console.log(cliente)
+
   return (
-    <div>
-      <p>Editar Cliente</p>
-    </div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Editar Cliente</h1>
+      <p className='mt-3'>Modifica los datos necesarios</p>
+
+      <div className='flex justify-end'>
+        <button
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+
+        {/* {errores?.length && errores.map(( error, i) => <Error key={i}>{error}</Error>)} */}
+
+        <Form
+           method='POST'
+           noValidate
+        >
+          <Formulario 
+            cliente={cliente}
+          />
+
+          <input 
+              type="submit"
+              className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+              value={'Registrar Cliente'}
+          />
+        </Form>
+      </div>
+    </>
   )
 }
 
